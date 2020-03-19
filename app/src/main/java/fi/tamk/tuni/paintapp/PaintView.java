96342cdefff928b4e17a2622d2e80496330f6219
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-19 21:10:52 +0200

    Create PaintView for the app drawing.

---
+++ app/src/main/java/fi/tamk/tuni/paintapp/PaintView.java
@@ -0,0 +1,13 @@
+package fi.tamk.tuni.paintapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PaintView extends View {
+    public PaintView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
