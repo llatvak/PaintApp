fc7111b4d538ca591c522bf753bdf6d279876cde
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-19 21:19:03 +0200

    Add paint view to MainActivity layout and class.

--- app/src/main/java/fi/tamk/tuni/paintapp/MainActivity.java
+++ app/src/main/java/fi/tamk/tuni/paintapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PaintView mPaintView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPaintView = (PaintView) findViewById(R.id.paint_view);
     }
 }
