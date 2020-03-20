6ff1a53cff109b9f4b855dbaa92fcfa54f5de005
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-20 13:12:49 +0200

    Modify UI elements and colors, remove support bar.

--- app/src/main/java/fi/tamk/tuni/paintapp/MainActivity.java
+++ app/src/main/java/fi/tamk/tuni/paintapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         mPaintView = (PaintView) findViewById(R.id.paint_view);
     }
