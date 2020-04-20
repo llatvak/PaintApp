b35d90e0abe2d3ac9fc30e744f8538c9d7c3dd03
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-20 13:53:28 +0300

    Add new activity when starting app.

---
+++ app/src/main/java/fi/tamk/tuni/paintapp/StartActivity.java
@@ -0,0 +1,14 @@
+package fi.tamk.tuni.paintapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_start);
+    }
+
+
+}
