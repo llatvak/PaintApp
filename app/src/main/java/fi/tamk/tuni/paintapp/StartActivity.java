79af5b30c17af658c3a10da8b14e518d9717f68a
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-20 14:25:51 +0300

    Add view elements to start drawing view.

--- app/src/main/java/fi/tamk/tuni/paintapp/StartActivity.java
+++ app/src/main/java/fi/tamk/tuni/paintapp/StartActivity.java
@@ -1,6 +1,10 @@
 package fi.tamk.tuni.paintapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
@@ -8,6 +12,12 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_start);
+    }
+
+    public void startClicked(View v) {
+        ImageButton b = findViewById(R.id.button_start);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 
 
