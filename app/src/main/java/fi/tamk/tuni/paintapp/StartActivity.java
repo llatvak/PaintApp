67bf8cb404d67da3a6161762aed0d42dea6861c0
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-20 14:52:05 +0300

    Add functionality to pick background color when starting new drawing.

--- app/src/main/java/fi/tamk/tuni/paintapp/StartActivity.java
+++ app/src/main/java/fi/tamk/tuni/paintapp/StartActivity.java
@@ -1,13 +1,14 @@
 package fi.tamk.tuni.paintapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -21,7 +22,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.layout_start);
         // Get rectangle template view to change background color to chosen/default
         mRectangleView = findViewById(R.id.myRectangleView);
-        mDefaultColor = ContextCompat.getColor(this, R.color.colorPrimary);
+        mDefaultColor = Color.WHITE;
         // Init show color button
         mButtonShowColors = findViewById(R.id.show_colors);
         mButtonShowColors.setOnClickListener(this::showColorPicker) ;
@@ -35,7 +36,19 @@ public class StartActivity extends AppCompatActivity {
 
     // Functionality of show color button
     public void showColorPicker(View v) {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
 
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+                mRectangleView.setBackgroundColor(mDefaultColor);
+            }
+        });
+        colorPicker.show();
     }
 
 
